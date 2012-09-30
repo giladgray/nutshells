7650284fbb9bfbb111415d2e2d6d293b5e8822b8
Gilad Gray <giladgray@gmail.com>
2012-09-29 17:53:06 -0700

    change up/down path helpers

--- app/helpers/nuts_helper.rb
+++ app/helpers/nuts_helper.rb
@@ -3,8 +3,8 @@ module NutsHelper
 		"""
 			<span class='label'>#{ nut.rating }</span>
 			<div class='btn-group btn-group-vertical'>
-				#{ link_to raw('<i class=\'icon-arrow-up\'></i>'), nuts_up_path(nut), remote: true, class: 'btn btn-mini' }
-				#{ link_to raw('<i class=\'icon-arrow-down\'></i>'), nuts_down_path(nut), remote: true, class: 'btn btn-mini' }
+				#{ link_to raw('<i class=\'icon-arrow-up\'></i>'), up_nut_path(nut), remote: true, class: 'btn btn-mini' }
+				#{ link_to raw('<i class=\'icon-arrow-down\'></i>'), down_nut_path(nut), remote: true, class: 'btn btn-mini' }
 			</div>
 		"""
 	end
