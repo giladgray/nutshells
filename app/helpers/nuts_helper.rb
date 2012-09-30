a8e2e7e1be8063db27437266483a4d68c856f083
Gilad Gray <giladgray@gmail.com>
2012-09-29 18:20:54 -0700

    improve nut display with long body text

--- app/helpers/nuts_helper.rb
+++ app/helpers/nuts_helper.rb
@@ -1,7 +1,7 @@
 module NutsHelper
 	def rating(nut)
 		"""
-			<span class='label'>#{ nut.rating }</span>
+			<span class='label #{'label-important' if nut.rating < 0 }'>#{ nut.rating }</span>
 			<div class='btn-group btn-group-vertical'>
 				#{ link_to raw('<i class=\'icon-arrow-up\'></i>'), up_nut_path(nut), remote: true, class: 'btn btn-mini' }
 				#{ link_to raw('<i class=\'icon-arrow-down\'></i>'), down_nut_path(nut), remote: true, class: 'btn btn-mini' }
